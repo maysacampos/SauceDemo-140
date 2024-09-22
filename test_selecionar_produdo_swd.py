2bb62b585072361d92f54ab60b8725559df78517
maysacampos <maysacampos_@hotmail.com>
2024-09-22 19:05:20 -0300

    Script iniciais feitos com Selenium IDE e manualmente  com Selenium WebDriver - fluxos de compra no site Saucedemo.com

--- test_selecionar_produdo_swd.py
+++ test_selecionar_produdo_swd.py
@@ -21,14 +21,38 @@ class Teste_Produtos():
         self.driver.get(self.url)                  # abre o navegador
         self.driver.find_element(By.ID, "user-name").send_keys("standard_user")             # escreve no campo user-name
         self.driver.find_element(By.NAME, "password").send_keys("secret_sauce")             # escreve a senha
-        self.driver.find_element(By.CSS_SELECTOR, "input.submit-button.btn_action").click()   # clique no botão de login
+        self.driver.find_element(By.CSS_SELECTOR, "input.submit-button.btn_action").click() # clique no botão de login
 
         # transição de página
 
-        assert self.driver.find_element(By.CSS_SELECTOR, ".title").text == "Products"  # confirma se está escrito Products no elemento
-        assert self.driver.find_element(By.ID, "item_4_title_link").text == "Sauce Labs Backpack" # confirma se é a mochila
-        # confirma o preço da mochila
+        assert self.driver.find_element(By.CSS_SELECTOR, ".title").text == "Products"              # confirma se está escrito Products no elemento
+        assert self.driver.find_element(By.ID, "item_4_title_link").text == "Sauce Labs Backpack"  # confirma se é a mochila
         assert self.driver.find_element(By.CSS_SELECTOR, ".inventory_item:nth-child(1) .inventory_item_price").text == (
             "$29.99" 
         )
+
+        # incluir produto no carrinho
         
+        self.driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click() #adiciona o produto no carrinho
+    
+        # entrar no carrinho
+        
+        self.driver.find_element(By.CSS_SELECTOR, ".shopping_cart_link").click() #entra no carrinho de compras
+        
+        # validar carrinho de compras
+        
+        assert self.driver.find_element(By.CSS_SELECTOR, ".shopping_cart_link").text == "1"
+
+        # validar produto no carrinho
+        
+        assert self.driver.find_element(By.CSS_SELECTOR, ".inventory_item_name").text == "Sauce Labs Backpack" #valida o nome da mochila
+        assert self.driver.find_element(By.CSS_SELECTOR, ".inventory_item_price").text == "$29.99" #valida o preço da mochila
+        
+       # Remover o produto
+       
+        self.driver.find_element(By.ID, "remove-sauce-labs-backpack").click() #remove a mochila do carrinho
+        
+       # Realizar Logout
+        
+        self.driver.find_element(By.ID, "react-burger-menu-btn").click() #entra no menu
+        self.driver.find_element(By.ID, "logout_sidebar_link").click() #faz log out no site
